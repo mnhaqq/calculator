b292f08d63c0faa4dd2c60b6fde74594d619eed2
Haqq Bassit <munagahmnh@gmail.com>
2024-01-07 17:52:43 +0000

    Build Calculator Functionality

--- script.js
+++ script.js
@@ -1,12 +1,68 @@
 let num1, num2, operator;
+let displayContent = "";
+let operandStack = [];
+let currentOperator;
+let operand;
 
-let mapOperators = {
+const mapOperators = {
     "+" : add,
     "-" : subtract,
     "*" : multiply,
-    "/" : divide
+    "/" : divide,
 }
 
+const displayArea = document.querySelector(".display");
+const buttons = document.querySelectorAll(".num");
+const clearButton = document.querySelector(".clear")
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (displayContent === "" && button.textContent === "-") {
+            displayContent += button.textContent
+            displayArea.textContent = displayContent;
+        } else if (button.textContent in mapOperators || button.textContent === "=") {
+            if (button.textContent in mapOperators) currentOperator = button.textContent;
+            displayArea.textContent = "";
+
+            if (!isNaN(displayContent) && !isNaN(parseFloat(displayContent))) {
+                operand = displayContent;
+                operandStack.push(operand);
+            }
+            
+            if (!(currentOperator in mapOperators)) {
+                displayArea.textContent = "Error!";
+                return;
+            };
+            if (operandStack.length == 2) {
+                let b = parseFloat(operandStack.pop());
+                let a = parseFloat(operandStack.pop());
+                let ans = operate(currentOperator, a, b);
+                if (ans === "Error!") {
+                    displayArea.textContent = ans;
+                    return;
+                } 
+                operandStack.push(ans);
+                if (String(ans).length > 3) displayArea.textContent = ans.toFixed(2);
+                else displayArea.textContent = ans;
+            }
+
+            if (button.textContent === "=") currentOperator = "";
+            displayContent = "";
+        } else {
+            displayContent += button.textContent
+            displayArea.textContent = displayContent;
+        }
+    });
+});
+
+clearButton.addEventListener('click', resetCalculator);
+
+function resetCalculator() {
+    operandStack = [];
+    displayContent = "";
+    currentOperator = "";
+    displayArea.textContent = "";
+}
 
 function operate(operator, num1, num2) {
     return mapOperators[operator](num1, num2);
@@ -18,4 +74,7 @@ function subtract(a, b) {return a - b;}
 
 function multiply(a, b) {return a * b;}
 
-function divide(a, b) {return a / b};
+function divide(a, b) {
+    if (b === 0) return "Error!";
+    return a / b;
+}
